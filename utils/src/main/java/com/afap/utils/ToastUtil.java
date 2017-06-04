468bfbe8af7349fa1ab6c3e06e9c681b6664c3af
AFAP_PC <814237762@qq.com>
2017-06-04 20:48:59 +0800

    增加sp,dp转px

--- utils/src/main/java/com/afap/utils/ToastUtil.java
+++ utils/src/main/java/com/afap/utils/ToastUtil.java
@@ -31,29 +31,6 @@ public class ToastUtil {
         }
     }
 
-
-    public static void showShort(Context context, String message) {
-        show(context, message, Toast.LENGTH_SHORT);
-    }
-
-    public static void showShort(Context context, int message) {
-        show(context, message, Toast.LENGTH_SHORT);
-    }
-
-    public static void show(Context context, int message, int duration) {
-        show(context, context.getString(message), duration);
-    }
-
-    public static void show(Context context, String message, int duration) {
-        if (null == toast) {
-            toast = Toast.makeText(context, message, duration);
-        } else {
-            toast.setDuration(duration);
-            toast.setText(message);
-        }
-        toast.show();
-    }
-
     public static void hideToast() {
         if (null != toast) {
             toast.cancel();
